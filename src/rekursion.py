a7213214b05bfece01a04640a1f4c7548772979a
Jürgen Schoenemeyer <108626845+jschoen42@users.noreply.github.com>
2025-01-03 16:04:46 +0100

    add wildcards for exclude list

--- src/rekursion.py
+++ src/rekursion.py
@@ -7,7 +7,11 @@
         - ancor path: relative/absolute path
         - folder, files, error paths: relative to ancor_path
 
+    PRIVAT:
+     - match_not_exclude( name: str, excludes: dict ) -> bool
+
 """
+import fnmatch
 
 from typing import Tuple
 from pathlib import Path
@@ -17,11 +21,20 @@ from utils.decorator import duration
 
 # exclude = {
 #     "folder": [".git", ".venv", "__pycache__", "__MACOSX"],
-#     "files": ["desktop.ini"]
+#     "files": ["desktop.ini", "folderico*.ico"]
 # }
 
+def match_not_exclude( name: str, excludes: dict ) -> bool:
+    if excludes is None:
+        return True
+
+    for exclude in excludes:
+        if fnmatch.fnmatch(name, exclude):
+            return False
+    return True
+
 @duration("{__name__} '{0}'")
-def get_filepaths_all( path: Path, exclude: dict = None ) -> Tuple[list, list, list]:
+def get_filepaths_all( path: Path, exclude: dict = {"folder": None, "files": None}, show_result: bool = True ) -> Tuple[list, list, list]:
     Trace.action(f"path '{path}'")
 
     files   = []
@@ -29,15 +42,13 @@ def get_filepaths_all( path: Path, exclude: dict = None ) -> Tuple[list, list, l
     errors  = []
 
     def get_filepaths( path: Path ) -> None:
-        nonlocal files, folders, errors
-
         try:
             for entry in path.iterdir():
                 if entry.is_file():
-                    if exclude is None or entry.name not in exclude["files"]:
-                        files.append( entry.name )
+                    if match_not_exclude(entry.name, exclude["files"]):
+                        files.append( entry.as_posix() )
                 elif entry.is_dir():
-                    if exclude is None or entry.name not in exclude["folder"]:
+                    if match_not_exclude(entry.name, exclude["folder"]):
                         folders.append( path.as_posix() )
                         get_filepaths( entry )
 
@@ -51,11 +62,16 @@ def get_filepaths_all( path: Path, exclude: dict = None ) -> Tuple[list, list, l
 
     get_filepaths( path )
 
-    Trace.result(f"folders: {len(folders)}, files: {len(files)}, errors: {len(errors)} - exclude: {exclude}")
-    return (files, folders, errors)
+    if len(errors)>0:
+        Trace.error(f"errors: {errors}")
+
+    if show_result:
+        Trace.result(f"folders: {len(folders)}, files: {len(files)}, errors: {len(errors)} - exclude: {exclude}")
+
+    return files, folders, errors
 
 @duration("{__name__} '{0}'")
-def get_filepaths_ancor( ancor_path: Path, exclude: dict = None ) -> Tuple[list, list, list]:
+def get_filepaths_ancor( ancor_path: Path, exclude: dict = {"folder": None, "files": None}, show_result: bool = True ) -> Tuple[list, list, list]:
     Trace.action(f"ancor '{ancor_path}'")
 
     files   = []
@@ -63,10 +79,7 @@ def get_filepaths_ancor( ancor_path: Path, exclude: dict = None ) -> Tuple[list,
     errors  = []
 
     def get_filepaths( rel_path: str ) -> None:
-        nonlocal files, folders, errors
-
         curr_path = ancor_path / rel_path
-
         try:
             for entry in Path(curr_path).iterdir():
                 if rel_path == "":
@@ -75,10 +88,10 @@ def get_filepaths_ancor( ancor_path: Path, exclude: dict = None ) -> Tuple[list,
                     name = rel_path + "/" + entry.name # as posix
 
                 if entry.is_file():
-                    if exclude is None or entry.name not in exclude["files"]:
+                    if match_not_exclude(entry.name, exclude["files"]):
                         files.append( name )
                 elif entry.is_dir():
-                    if exclude is None or entry.name not in exclude["folder"]:
+                    if match_not_exclude(entry.name, exclude["folder"]):
                         folders.append( name )
                         get_filepaths( name )
 
@@ -95,5 +108,7 @@ def get_filepaths_ancor( ancor_path: Path, exclude: dict = None ) -> Tuple[list,
     if len(errors)>0:
         Trace.error(f"errors: {errors}")
 
-    Trace.result(f"folders: {len(folders)}, files: {len(files)}, errors: {len(errors)} - exclude: {exclude}")
-    return (files, folders, errors)
+    if show_result:
+        Trace.result(f"folders: {len(folders)}, files: {len(files)}, errors: {len(errors)} - exclude: {exclude}")
+
+    return files, folders, errors
